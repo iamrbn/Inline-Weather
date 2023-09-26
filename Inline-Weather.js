d3c3e9600e3384e86b2b54cda6b28d3710055c8e
Robinson <75579375+iamrbn@users.noreply.github.com>
2023-09-26 15:03:31 +0200

    Script Version 1.0

---
+++ Inline-Weather.js
@@ -0,0 +1,11 @@
+//++++++++++++++++ START CONFIG AREA ++++++++++++++++++++++
+
+const unit = 'metric' //Units of measurement: 'standard', 'metric' and 'imperial' units are available.
+const unitSymb = '°' //Celsius
+const unitSpeed = 'km/h'
+const language = 'de' //learn more: https://openweathermap.org/current#multi
+const apiKey = "QWERTZ1234567"
+const standardParameter = "60;current"
+
+//++++++++++++++++ END CONFIG AREA ++++++++++++++++++++++++
+
